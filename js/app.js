992f86a6fde4a179dba7e373aee1d34217a27087
Alexander Starchenko <duxard@gmail.com>
2016-10-31 11:47:43 +0300

    fifth

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation()
+
+var upper = $("#up");
+
+$(document).scroll(function(){
+    if($(this).scrollTop() > 90){
+        upper.fadeIn(300);
+    }else{
+        upper.fadeOut(100);
+    }
+});
+
+upper.on('click',function(){
+    $("body,html").animate({"scrollTop":0}, 200);
+});
